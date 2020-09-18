dab4cc75a2f2a22609668f036b813a22b371e370
Your Name <you@example.com>
2020-09-19 00:27:53 +0300

    Updates

---
+++ precache-manifest.c004f414f2ed14d5b816267ad0065b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b12108b50376ea14b73f183ef2ffb86",
+    "url": "/ZMovies-Forum/index.html"
+  },
+  {
+    "revision": "f7481d5526ced62346cc",
+    "url": "/ZMovies-Forum/static/css/main.127684a8.chunk.css"
+  },
+  {
+    "revision": "e5c2ddadc3b8289f897b",
+    "url": "/ZMovies-Forum/static/js/2.1a7f7d90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ZMovies-Forum/static/js/2.1a7f7d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7481d5526ced62346cc",
+    "url": "/ZMovies-Forum/static/js/main.6bb76a19.chunk.js"
+  },
+  {
+    "revision": "b0057f67819590746fa5",
+    "url": "/ZMovies-Forum/static/js/runtime-main.6eb8a3aa.js"
+  }
+]);
